cbcaba4a773b2c282548f13dbd18a2e16767c1c2
Elliott M Austin <elliott@smartaustin.com>
2023-06-28 19:23:51 -0700

    Bug fixes and added

--- JavatoBedrockconverter.sh
+++ JavatoBedrockconverter.sh
@@ -1,3 +1,6 @@
 cd ./converted_resource_pack
 mv ./pack.png ./pack_icon.png
-mv ./assets/minecraft/textures/block ./assets
+mv ./assets/minecraft/textures/block ./
+mv ./block ./blocks
+cd ./blocks
+mv ./yellow_wool.png ./wool_colored_yellow
